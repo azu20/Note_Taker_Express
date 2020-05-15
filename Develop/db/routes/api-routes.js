1a17b4cc80dacc2a93c22e7a1e419baca7608fe0
Azucena Herrera <azucenaphx@gmail.com>
2020-05-15 00:05:00 -0700

    created api and html routes

---
+++ Develop/db/routes/api-routes.js
@@ -0,0 +1,39 @@
+const data = require("../note-data");
+
+module.exports = function (app) {
+    app.get("/api/notes", function (request, response){
+        console.log(`/api/notes called`);
+        response.json(notes); 
+    }); 
+
+    app.get("/api/notes/:id", function (request, response) {
+        console.log(`api/notes/:id${request.params.id} called`);
+
+        let noteId = request.params.id;
+
+        for (let i = 0; i < notes.length; i ++) {
+            if (notes[i].id === notesId) {
+                console.log(notes[i]);
+
+                return response.json(notes[i]);
+            }
+
+            return response.json(false); 
+        }
+    });
+
+    app.post("/api/notes", function (request, response) {
+        console.log(`POST /api/notes called`); 
+
+        const newNote = request.body;
+       newNote.id = (request.body.name.split (""))[0].toLowerCase(); 
+    
+        console.log(newNote); 
+
+        characters.push(newNote); 
+
+        response.json(newNote); 
+       
+        
+    }); 
+};
---
+++ Develop/db/routes/html-routes.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = function (app) {
+    app.get("/", function (request, response) {
+        console.log(`/ called`);
+        response.sendFile(path.join(_direnam, "../public/index.html"));
+    });
+
+    app.get("/app", function (request, response) {
+        response.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+}; 
