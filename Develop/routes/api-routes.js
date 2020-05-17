d7c9f0b9ced6bd3a4f4aa02c1ae5d3099088daac
Azucena Herrera <azucenaphx@gmail.com>
2020-05-16 20:11:16 -0700

    created manager outside server = IIFE

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,49 +1,83 @@
 var fs = require("fs");
 const path = require("path");
-const notesDB = "../db/db.json";
+// const DBpath =  require("../db/db.json");
+
+const dbManager = ( 
+       function(path){
+            console.log("The function which initializes dbManager has run.");
+        
+            let notesDB = [];
+            fs.readFile((__dirname, "./develop/db/db.json"), 'utf8',  (err, fileContents) => {
+                if (err) {
+                    console.log("File read failed:", err)
+                    return;
+                }
+                notesDB = fileContents;
+               console.log(notesDB);
+            });
+            console.log(notesDB);
 
 
+            function add(note){
+
+                return note;
+            };
+            function remove(note){
+                console.log(path);
+                return note;
+            }
+            return {
+                "addNote": add,
+                "deleteNote": remove
+            };
+        }(path)
+);
 
 module.exports = function (app) {
-    app.get("/test", function (request, response) {
-        // console.log(`/ called`);
-        response.sendFile(path.join(__dirname, notesDB));
-        // response.sendFile(__dirname + "/public/index.html");
+
+    app.post("/api/note", function (request, response) {
+        
+        const thenote = request.body;
+
+        const note = dbManager.addNote(thenote);
+
+        return response.json(note); 
+
     });
     
-    app.get("/api/notes", function (request, response){
-        console.log(`/api/notes called`);
-        response.json(notes); 
-    }); 
+    // app.get("/api/notes", function (request, response){
+    //     console.log(`/api/notes called`);
+    //     response.json(notes); 
+    // }); 
 
-    app.get("/api/notes/:id", function (request, response) {
-        console.log(`api/notes/:id${request.params.id} called`);
+    // app.get("/api/notes/:id", function (request, response) {
+    //     console.log(`api/notes/:id${request.params.id} called`);
 
-        let noteId = request.params.id;
+    //     let noteId = request.params.id;
 
-        for (let i = 0; i < notes.length; i ++) {
-            if (notes[i].id === notesId) {
-                console.log(notes[i]);
+    //     for (let i = 0; i < notes.length; i ++) {
+    //         if (notes[i].id === notesId) {
+    //             console.log(notes[i]);
 
-                return response.json(notes[i]);
-            }
+    //             return response.json(notes[i]);
+    //         }
 
-            return response.json(false); 
-        }
-    });
+    //         return response.json(false); 
+    //     }
+    // });
 
-    app.post("/api/notes", function (request, response) {
-        console.log(`POST /api/notes called`); 
+    // app.post("/api/notes", function (request, response) {
+    //     console.log(`POST /api/notes called`); 
 
-        const newNote = request.body;
-       newNote.id = (request.body.name.split (""))[0].toLowerCase(); 
+    //     const newNote = request.body;
+    //    newNote.id = (request.body.name.split (""))[0].toLowerCase(); 
     
-        console.log(newNote); 
+    //     console.log(newNote); 
 
-        notes.push(newNote); 
+    //     notes.push(newNote); 
 
-        response.json(newNote); 
-    }); 
+    //     response.json(newNote); 
+    // }); 
     
     //To DO: add DELETE, 
 };
